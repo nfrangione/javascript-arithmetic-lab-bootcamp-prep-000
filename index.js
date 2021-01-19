491f4939241f66d547aad1a9784d4110126ed818
Nicholas Frangione <nfrangione8@gmail.com>
2021-01-19 23:23:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,9 @@ function increment(n) {
 function decrement(n) {
   return n -= 1
 }
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+function preserveDecimal(string) {
+  return parseFloat(string)
+}
